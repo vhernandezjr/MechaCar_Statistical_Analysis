2ad5c43de5484fbc7ebaeb90d9161dfedd57f22c
Victor Hernandez <vhernandezjr@gmail.com>
2022-07-01 22:05:52 -0500

    inital push

---
+++ MechaCarChallenge.R
@@ -0,0 +1,9 @@
+# load package
+library(dplyr)
+# import and read csv as a dataframe
+MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+head(MechaCar_mpg)
+# linear regression model
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_mpg)
+# summary statistics
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_mpg))
